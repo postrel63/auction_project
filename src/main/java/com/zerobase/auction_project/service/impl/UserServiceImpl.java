94160beddefe72b78a03685f40d2273c2448e821
postrel63 <postrel63@naver.com>
2023-09-06 09:52:11 +0900

    오타 수정

--- src/main/java/com/zerobase/auction_project/service/impl/UserServiceImpl.java
+++ src/main/java/com/zerobase/auction_project/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
