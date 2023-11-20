f3729993c4f138ba1dfca7372247d9a9e9657c95
postrel63 <postrel63@naver.com>
2023-11-21 06:11:33 +0900

    feat. 어노테이션 정리

--- src/main/java/com/zerobase/auction_project/AuctionProjectApplication.java
+++ src/main/java/com/zerobase/auction_project/AuctionProjectApplication.java
@@ -3,18 +3,12 @@ package com.zerobase.auction_project;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ServletComponentScan
 @EnableFeignClients
 @SpringBootApplication
-@RequiredArgsConstructor
 @EnableJpaAuditing
-@EnableJpaRepositories
 public class AuctionProjectApplication {
 
     public static void main(String[] args) {
