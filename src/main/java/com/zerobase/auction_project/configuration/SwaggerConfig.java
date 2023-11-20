e337107397b134e769445bf0bae3c15c92632e8a
postrel63 <postrel63@naver.com>
2023-11-21 06:10:39 +0900

    feat. swagger 오류로 일단 주석처리

---
+++ src/main/java/com/zerobase/auction_project/configuration/SwaggerConfig.java
@@ -0,0 +1,60 @@
+//package com.zerobase.auction_project.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.ApiKey;
+//import springfox.documentation.service.AuthorizationScope;
+//import springfox.documentation.service.SecurityReference;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spi.service.contexts.SecurityContext;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(true)
+//                .apiInfo(apiInfo())
+//                .securitySchemes(List.of(apiKey()))
+//                .securityContexts(List.of(securityContext()))
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.zerobase.auction_project"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    public ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("TRIPTALK")
+//                .description("")
+//                .version("0.1")
+//                .build();
+//    }
+//
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(defaultAuth())
+//                .build();
+//    }
+//
+//    private List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return List.of(new SecurityReference("Authorization", authorizationScopes));
+//    }
+//
+//    // ApiKey 정의
+//    private ApiKey apiKey() {
+//        return new ApiKey("Authorization", "Authorization", "header");
+//    }
+//
+//}
