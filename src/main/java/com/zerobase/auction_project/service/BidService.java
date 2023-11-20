d2e35d0a96085a23af6d7c1e03949f85585e4b78
postrel63 <postrel63@naver.com>
2023-11-21 04:11:33 +0900

    feat. 낙찰자를 선정하는 bidUser 메서드 생성

--- src/main/java/com/zerobase/auction_project/service/BidService.java
+++ src/main/java/com/zerobase/auction_project/service/BidService.java
@@ -1,5 +1,7 @@
 package com.zerobase.auction_project.service;
 
+import com.zerobase.auction_project.domain.Bid;
+import com.zerobase.auction_project.domain.User;
 import com.zerobase.auction_project.domain.request.BidForm;
 import com.zerobase.auction_project.domain.response.BidResponse;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,6 @@ public interface BidService {
 
     void bidEnded();
 
-
+    Bid bidUser(Long auctionId);
 
 }
