25e398a6d0c424ba07ea6b6331ca86c2a365a3d0
postrel63 <postrel63@naver.com>
2023-11-21 04:10:23 +0900

    feat. 입찰 기록 존재 여부를 판단하는 existsByAuction_Id와 낙찰자를 찾는 findFirstByAuction_IdOrderByPriceDesc 생성

--- src/main/java/com/zerobase/auction_project/repository/BidRepository.java
+++ src/main/java/com/zerobase/auction_project/repository/BidRepository.java
@@ -1,9 +1,20 @@
 package com.zerobase.auction_project.repository;
 
 import com.zerobase.auction_project.domain.Bid;
+import com.zerobase.auction_project.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface BidRepository extends JpaRepository<Bid,String> {
-//}
+import java.util.List;
+
+@Repository
+public interface BidRepository extends JpaRepository<Bid, String> {
+
+
+    List<Bid> findByAuction_Id(Long auctionId);
+
+    Boolean existsByAuction_Id(Long auctionId);
+
+    Bid findFirstByAuction_IdOrderByPriceDesc (Long auctionId);
+
+}
