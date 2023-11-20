9d705dedd6be7fef087ad527b4968ab259fab10e
postrel63 <postrel63@naver.com>
2023-11-21 02:21:55 +0900

    feat. exception을 각각 code, custom, handler 패키지로 구분

--- src/main/java/com/zerobase/auction_project/exception/handler/ExceptionController.java
+++ src/main/java/com/zerobase/auction_project/exception/handler/ExceptionController.java
@@ -1,6 +1,8 @@
-package com.zerobase.auction_project.exception;
+package com.zerobase.auction_project.exception.handler;
 
 
+import com.zerobase.auction_project.exception.code.ErrorCode;
+import com.zerobase.auction_project.exception.custom.CustomException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,9 +16,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionController {
 
 
-    @ExceptionHandler({
-            CustomException.class
-    })
+    @ExceptionHandler({ CustomException.class})
     public ResponseEntity<ExceptionResponse> customRequestException(final CustomException c) {
         log.warn("api Exception: {} ", c.getErrorcode());
         return ResponseEntity.badRequest().body(new ExceptionResponse(c.getMessage(), c.getErrorcode()));
